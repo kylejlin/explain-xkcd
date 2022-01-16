e73928e4cba30b87e6240074e8e586b04419b375
Kenneth Bingham <w@qrk.us>
2022-01-16 13:51:07 -0500

    also handle case where comic landing page is in focus

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
   chrome.tabs.create({
     url:
-      "https://www.explainxkcd.com/wiki/index.php/" + tab.url.replace(/^https?:\/\/xkcd\.com\/(\d+)\/?.*$/gi, "$1")
+      "https://www.explainxkcd.com/wiki/index.php/" + tab.url.replace(/^https?:\/\/xkcd\.com(?:\/(\d*)\/?.*)?$/gi, "$1")
   });
 });
